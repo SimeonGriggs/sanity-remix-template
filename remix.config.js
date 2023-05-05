8f6ff755635a12bfb88e6d4a2d6a4658026d42e4
Simeon Griggs <simeon@hey.com>
2023-05-05 11:36:59 +0100

    remix 1.16 upgrades

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  serverModuleFormat: 'cjs',
+  tailwind: true,
   future: {
-    unstable_tailwind: true,
     v2_errorBoundary: true,
     v2_normalizeFormMethod: true,
     v2_meta: true,
