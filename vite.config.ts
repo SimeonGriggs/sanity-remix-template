cf1294f025c711d74ef918f363560381074a0213
Simeon Griggs <simeon@hey.com>
2024-03-01 10:16:12 +0000

    add resolid

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {vitePlugin as remix} from '@remix-run/dev'
 import {installGlobals} from '@remix-run/node'
+import {vercelServerlessPreset} from '@resolid/remix-plugins/vercel-serverless'
 import {defineConfig} from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
@@ -9,7 +10,23 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      presets: [
+        vercelServerlessPreset({
+          regions: 'iad1',
+          cleanUrls: true,
+          cacheFiles: [
+            'favicon.svg',
+            'apple-touch-icon.png',
+            'manifest.webmanifest',
+          ],
+          cacheFolders: ['icons', 'images'],
+        }),
+      ],
+    }),
+    tsconfigPaths(),
+  ],
   optimizeDeps: {
     exclude: ['@resvg/resvg-js'],
   },
