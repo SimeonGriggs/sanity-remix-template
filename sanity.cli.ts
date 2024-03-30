ee965748e95704d98b2499a22e43e0b9b7eb93e6
Simeon Griggs <simeon@hey.com>
2024-03-30 06:52:16 +0000

    removing module allows script to run

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,8 @@
 import {defineCliConfig} from 'sanity/cli'
 
-// We can assume this file will only be read in a Node.js environment
+// We can assume this file will only
+// be read in a Node.js environment
+// where process is defined
 export default defineCliConfig({
   api: {
     projectId: process.env.VITE_SANITY_PROJECT_ID,
