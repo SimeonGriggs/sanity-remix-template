9e4cc40cb867a8490664c6a4b57caa36f15ea981
Simeon Griggs <simeon@hey.com>
2024-06-17 10:02:39 +0100

    prevent reloading by moving client outside of function scope

--- app/components/SanityLiveMode.tsx
+++ app/components/SanityLiveMode.tsx
@@ -3,17 +3,17 @@ import {useLiveMode} from '@sanity/react-loader'
 import {client} from '~/sanity/client'
 import {STUDIO_BASEPATH} from '~/sanity/constants'
 
+const liveClient = client.withConfig({
+  stega: {
+    enabled: true,
+    studioUrl: STUDIO_BASEPATH,
+  },
+})
+
 // Default export required for lazy loading
 // eslint-disable-next-line import/no-default-export
 export default function SanityLiveMode() {
-  useLiveMode({
-    client: client.withConfig({
-      stega: {
-        enabled: true,
-        studioUrl: STUDIO_BASEPATH,
-      },
-    }),
-  })
+  useLiveMode({client: liveClient})
 
   return null
 }
